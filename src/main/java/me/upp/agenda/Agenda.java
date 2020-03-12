bf9fbadc6f2866afbfa860e7eae8a1ea1685fb30
ImSrPanda-LAPTOP <oscardali370@gmail.com>
2020-03-12 10:48:35 -0600

    Dali | Creacion de clases

--- src/main/java/me/upp/agenda/Agenda.java
+++ src/main/java/me/upp/agenda/Agenda.java
@@ -1,13 +1,9 @@
 package me.upp.agenda;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.ArrayList;
 
 public class Agenda {
 
-    @Getter @Setter
     private ArrayList<String> agendar = new ArrayList<String>();
 
 }
---
+++ src/main/java/me/upp/agenda/Contacto.java
@@ -0,0 +1,4 @@
+package me.upp.agenda;
+
+public class Contacto {
+}
