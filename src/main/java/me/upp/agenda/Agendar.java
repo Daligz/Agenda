718aea9749c2d38974f8fb6e3d2ef1feb17189ed
pallito0316 <pallito0316@gmail.com>
2020-03-17 20:48:50 -0600

    Paola |  variables getters y setters

--- src/main/java/me/upp/agenda/Agendar.java
+++ src/main/java/me/upp/agenda/Agendar.java
@@ -1,4 +1,40 @@
 package me.upp.agenda;
 
 public class Agendar {
+    private String nota;
+    private date fecha;
+    private Contacto contacto;
+
+    public Agendar(String nota, date fecha, Contacto contacto) {
+        this.nota = nota;
+        this.fecha = fecha;
+        this.contacto = contacto;
+    }
+
+    
+    public String getNota() {
+        return nota;
+    }
+
+    public void setNota(String nota) {
+        this.nota = nota;
+    }
+
+    public date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(date fecha) {
+        this.fecha = fecha;
+    }
+
+    public Contacto getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(Contacto contacto) {
+        this.contacto = contacto;
+    }
+    
+    
 }
